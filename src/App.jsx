f4d080b1d4bb5378cb12fe5f665cfd15168b54e1
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-09-16 22:26:52 +0600

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen flex items-center justify-center text-center py-12">
       <div>
-        <h1 className="text-3xl md:text-5xl font-bold">Website Under Development</h1>
-        <p className="mt-3">We`re working hard to bring you our new website. Stay tuned!</p>
+        <h3 className="text-3xl md:text-5xl font-bold">Website Under Development</h3>
+        <p className="m-3">We`re working hard to bring you our new website. Stay tuned!</p>
         <CountdownTimer />
         <PlaceholderPage />
       </div>
